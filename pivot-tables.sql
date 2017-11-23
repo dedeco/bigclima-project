c3f734d9d85452dfe827ba9a6950d49eb2523f2a
Andre <dedeco@gmail.com>
2017-11-23 11:37:51 -0200

    Example pivoting a table 2

--- pivot-tables.sql
+++ pivot-tables.sql
@@ -1,3 +1,4 @@
+/* query to get parameters from a id station */
 SELECT measurements.measure_date_complete AS measurements_measure_date_complete ,
        measurements.measure_date AS measurements_measure_date ,
        measurements.utc_hour AS measurements_utc_hour ,
@@ -17,6 +18,7 @@ WHERE weather_stations.id = 363
 order by measurements.measure_date_complete,measurements.parameter_id
 LIMIT 4
 
+/* pivot table */
 CREATE TEMP TABLE PIVOT363 as
 Select *
 from crosstab (
@@ -31,8 +33,7 @@ order by measurements.measure_date_complete,measurements.parameter_id',
 'select name from PARAMETERS order by id'
 ) as FINAL_RESULT(mdate DATE, prec NUMERIC, pa NUMERIC, pa_amax NUMERIC, pa_min NUMERIC,ra NUMERIC, temp_ar NUMERIC, temp_orv NUMERIC, temp_max NUMERIC, temp_max_orv NUMERIC, temp_min NUMERIC, temp_min_orv NUMERIC, umid NUMERIC, umid_max NUMERIC, umid_min NUMERIC, vento_vel NUMERIC, vento_dir NUMERIC, vento_raj NUMERIC);
 
-
-
+/* query measurements per id and date and parameter_id*/
 SELECT *
 FROM measurements
 WHERE weather_station_id = 363
@@ -40,27 +41,3 @@ WHERE weather_station_id = 363
   AND measure_date_complete <= '2008-06-07'
   AND parameter_id = 2;
 
-select name from PARAMETERS order by parameter_id
-
-
-SELECT * 
-FROM crosstab( 'select student, subject, evaluation_result from evaluations order by 1,2') 
-     AS final_result(Student TEXT, Geography NUMERIC,History NUMERIC,Language NUMERIC,Maths NUMERIC,Music NUMERIC);
-
-Precipitação
- Pressão Atmosferica
- Pressão Atmosférica Máxima
- Pressão Atmosférica Mínima
- Radiacao Global
- Temperatura Do Ar
- Temperatura Do Ponto De Orvalho
- Temperatura Máxima
- Temperatura Máxima Do Ponto De Orvalho
- Temperatura Mínima
- Temperatura Mínima Do Ponto De Orvalho
- Umidade Relativa Do Ar
- Umidade Relativa Maxima Do Ar
- Umidade Relativa Minima Do Ar
- Vento Velocidade
- Vento, Direção
- Vento, Rajada Maxima
