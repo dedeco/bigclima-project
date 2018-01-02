1ff34bc37c70147df20152c896b9ff64bba292c1
André Araújo <dedeco@gmail.com>
2018-01-02 18:24:15 -0200

    Fix the export csv

--- export_to_csv.py
+++ export_to_csv.py
@@ -1,32 +1,32 @@
 ﻿#!/usr/bin/env python
 
 from database import *
+from slugify import slugify
 
 import csv
 
 def export():
 	
-	q = session.query(WeatherStation).all()
+	q = session.query(WeatherStation).filter_by(id=329)
 
 	for ws in q.all():
 
 		q = session.query(WeatherStation.name, MeasurementHourly)
-		q = q.join(WeatherStation, WeatherStation.id == MeasurementHourly.wsid)
-		q = q.filter(MeasurementHourly.wsid == ws.id)
+		q = q.join(MeasurementHourly, MeasurementHourly.wsid == WeatherStation.id)
+		q = q.filter(WeatherStation.id == ws.id)
+		q = q.order_by(MeasurementHourly.mdct)
 
-		file = '.data/' + ws.name + '.csv'
+		file = './data/' + slugify(ws.name) + '.csv'
 
-		outfile = open(file, 'wb')
-		outcsv = csv.writer(outfile)
 
-		header = [u'ws-name','prcp','stp','smax','smin','gbrd','temp','tmax','tmin','dewp','dmax','dmin','hmdy','hmax','hmin','wdsp','wdct','gust']
-		
-		outcsv.writerow(header)		
+		with open(file, 'w') as csvfile:
+			outcsv = csv.writer(csvfile, delimiter=',',quotechar='"', quoting = csv.QUOTE_MINIMAL)
+			
+			header = ['date_complete','date','hour','prcp','stp','smax','smin','gbrd','temp','tmax','tmin','dewp','dmax','dmin','hmdy','hmax','hmin','wdsp','wdct','gust']
+			outcsv.writerow(header)		
 
-		for r in q.all():			
-			outcsv.writerow([r.name,r.prcp,r.stp,r.smax,r.smin,r.gbrd,r.temp,r.tmax,r.tmin,r.dewp,r.dmax,r.dmin,r.hmdy,r.hmax,r.hmin,r.wdsp,r.wdct,r.gust])
-	
-	outfile.close()			
+			for name, r in q.all():
+				outcsv.writerow([r.mdct,r.mdct,r.uthr,r.prcp,r.stp,r.smax,r.smin,r.gbrd,r.temp,r.tmax,r.tmin,r.dewp,r.dmax,r.dmin,r.hmdy,r.hmax,r.hmin,r.wdsp,r.wdct,r.gust])	
 
 if __name__ == "__main__":
 	export()
