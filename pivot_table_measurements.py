7688e9b22dc01398f6528477f17260ed3f356ec9
André Araújo <dedeco@gmail.com>
2018-01-07 17:09:00 -0200

    Improving the pivot script

--- pivot_table_measurements.py
+++ pivot_table_measurements.py
@@ -1,12 +1,13 @@
 from database import *
-from sqlalchemy import and_
+from sqlalchemy import and_, func
 
 import pandas as pd
 import datetime
+import sqlalchemy
 
 def main():
-	#q = session.query(MeasureRanges).filter(MeasureRanges.wsid==329)
-	q = session.query(MeasureRanges).order_by(MeasureRanges.wsid) 
+	q = session.query(MeasureRanges).filter(MeasureRanges.wsid>=32).order_by(MeasureRanges.wsid) 
+	#q = session.query(MeasureRanges).order_by(MeasureRanges.wsid) 
 
 	ranges = q.all()
 
@@ -16,47 +17,61 @@ def main():
 
 		print ('Pivoting WS %s' %r.wsid)
 
+		try:		
+			m = session.query(func.max(MeasurementHourly.mdct)).filter(MeasurementHourly.wsid==r.wsid).scalar()
+			print ('Exist min: %s' %m)			
+		except NoResultFound:
+			m = r.min
+
+		if m < r.min:
+			m = r.min
+
 		dates = pd.date_range(r.min,r.max, freq="H")
 
 		for d in dates:
-			m = session.query(Measurement).filter(and_(Measurement.mdth==d,Measurement.wsid==r.wsid)).all()
-			
-			values = [(i.ptid,i.vlue) for i in m]
-			l = dict(values)
 
-			mh = MeasurementHourly()
-			mh.wsid = r.wsid
-			mh.mdct = d
-			mh.msdt = d 
-			mh.uthr = d.strftime("%H:%M:%S")
-			mh.prcp = l.get(1)
-			mh.stp = l.get(2)
-			mh.smax = l.get(3)
-			mh.smin = l.get(4)
-			mh.gbrd = l.get(5)
-			mh.temp = l.get(6)
-			mh.tmax = l.get(7)
-			mh.tmin = l.get(8)
-			mh.dewp = l.get(9)
-			mh.dmax = l.get(10)
-			mh.dmin = l.get(11)
-			mh.hmdy = l.get(12)
-			mh.hmax = l.get(13)
-			mh.hmin = l.get(14)
-			mh.wdsp = l.get(15)
-			mh.wdct = l.get(16)
-			mh.gust = l.get(17)
+			try:
+				m = session.query(Measurement).filter(and_(Measurement.mdth==d,Measurement.wsid==r.wsid)).all()
+				
+				values = [(i.ptid,i.vlue) for i in m]
+				l = dict(values)
 
-			session.add(mh)
+				mh = MeasurementHourly()
+				mh.wsid = r.wsid
+				mh.mdct = d
+				mh.msdt = d 
+				mh.uthr = d.strftime("%H:%M:%S")
+				mh.prcp = l.get(1)
+				mh.stp = l.get(2)
+				mh.smax = l.get(3)
+				mh.smin = l.get(4)
+				mh.gbrd = l.get(5)
+				mh.temp = l.get(6)
+				mh.tmax = l.get(7)
+				mh.tmin = l.get(8)
+				mh.dewp = l.get(9)
+				mh.dmax = l.get(10)
+				mh.dmin = l.get(11)
+				mh.hmdy = l.get(12)
+				mh.hmax = l.get(13)
+				mh.hmin = l.get(14)
+				mh.wdsp = l.get(15)
+				mh.wdct = l.get(16)
+				mh.gust = l.get(17)
 
-			i += 1
-			if i > 10000:
-				print ('+ 10000 records commited!')
-				session.commit()
-				i=0
+				session.add(mh)
+
+				i += 1
+				if i > 10000:
+					print ('+ 10000 records commited!')
+					session.commit()
+					i=0
+
+			except sqlalchemy.exc.IntegrityError:
+				print ('duplicate date: %s' %d)
+				session.rollback()
 
 		session.commit()
-
 
 if __name__ == "__main__":
 	main()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 import re
+import time
 
 def dms2dd(degrees, minutes, seconds, direction):
     dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60);
@@ -19,3 +20,16 @@ def parse_dms(dms):
     lat = dms2dd(parts[0], parts[1], parts[2], parts[3])
     lng = dms2dd(parts[4], parts[5], parts[6], parts[7])
     return (lat, lng)
+
+def datestring_to_date(datestr, formatstr="%Y-%m-%d %H:%M:%S"):
+    try:
+        dt = time.strptime(datestr, formatstr)
+    except ValueError:
+        dt = None
+
+    if dt:
+        dd = datetime.date(*dt[:3])
+    else:
+        dd = None
+
+    return dd
