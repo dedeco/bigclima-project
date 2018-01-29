939de3dbdeaff3cad7d746c421d633af4fc17c0e
André Araújo <dedeco@gmail.com>
2018-01-29 01:20:55 -0200

    csv to plot maps lat and lon

--- export_ws_to_plot_csv.py
+++ export_ws_to_plot_csv.py
@@ -8,17 +8,17 @@ def export():
 
 	q = session.query(WeatherStation)
 
-	file = './data/weather-station-brasil.csv'
+	file = './data/weather-station-brasil_only.csv'
 
 
 	with open(file, 'w') as csvfile:
 		outcsv = csv.writer(csvfile, delimiter=',',quotechar='"', quoting = csv.QUOTE_MINIMAL)
 
-		header = ['id','name','lat','lon'] 
+		header = ['lat','lon'] 
 		outcsv.writerow(header)		
 
 		for r in q.all():
-			outcsv.writerow([r.id,r.name,r.lat,r.lon])	
+			outcsv.writerow([r.lat,r.lon])	
 
 if __name__ == "__main__":
 	export()
