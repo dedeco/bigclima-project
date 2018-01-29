ca98459b553dba486f59a28525f0fa016b441a77
Andre <dedeco@gmail.com>
2018-01-29 01:25:34 -0200

    script invert lat and lon

--- export_ws_to_plot_csv.py
+++ export_ws_to_plot_csv.py
@@ -14,11 +14,12 @@ def export():
 	with open(file, 'w') as csvfile:
 		outcsv = csv.writer(csvfile, delimiter=',',quotechar='"', quoting = csv.QUOTE_MINIMAL)
 
-		header = ['lat','lon'] 
-		outcsv.writerow(header)		
+		#header = ['lat','lon'] 
+		#outcsv.writerow(header)		
 
 		for r in q.all():
-			outcsv.writerow([r.lat,r.lon])	
+			if r.lat > 0.0 and r.lon > 0.0:
+				outcsv.writerow([r.lon,r.lat])	
 
 if __name__ == "__main__":
 	export()
