2f4c3590b7fa0a83ab94c36dd417ac375447ea20
André Araújo <dedeco@gmail.com>
2018-02-11 15:53:56 -0200

    some adjusts algortihm to find extreme rain

--- find_extreme_prpc.py
+++ find_extreme_prpc.py
@@ -10,14 +10,17 @@ def export():
 	q = q.join(City, WeatherStation.cities_id == City.id)
 	q = q.join(State, City.state_id == State.id)
 	q = q.filter(State.abbreviation.in_(['MG','SP','ES','RJ']))
-	q = q.filter(WeatherStation.id == 329)
-	q = q.order_by(WeatherStation.wsid)
+	#q = q.filter(WeatherStation.id == 329)
+	q = q.order_by(WeatherStation.id)
 
 	for ws in q.all():
 
 		q = session.query(MeasurementHourly.mdct, MeasurementHourly.prcp)
 		q = q.filter(MeasurementHourly.wsid == ws.id)
 		q = q.order_by(MeasurementHourly.mdct)
+
+
+		print('Finding extreme for:%s' %ws.name)
 
 		data = q.all()
 
@@ -29,10 +32,13 @@ def export():
 
 			end = start + 24
 			#print (start,end)
-			prcp = [x[1] for x in data[start:end]]
+			#_prcp = [x[1] for x in data[start:end]]
+			prcp = [x[1] for x in data[start:end] if x[1]]
+			#print (prcp)
+
 			t = sum(prcp)
 
-			if sum(prcp) > 50.0:
+			if t > 50.0:
 
 				str_time = [x[0] for x in data[start:end]][0] 
 				end_time = [x[0] for x in data[start:end]][-1]
@@ -42,6 +48,9 @@ def export():
 				e.mdct_str = str_time
 				e.mdct_end = end_time
 				e.prcp = t
+
+				#print('Found it',t,str_time,end_time)
+				#print(_prcp)
 
 				session.add(e)
 				session.commit()
